7355a23483549cd42c54d1ddc8f52727234d8b20
Faiz <sycopaat22@gmail.com>
2025-06-04 22:26:05 +0700

    aa

--- veggie/vite.config.js
+++ veggie/vite.config.js
@@ -2,7 +2,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://poorly-real-ghoul.ngrok-free.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         // Tambahkan konfigurasi berikut:
